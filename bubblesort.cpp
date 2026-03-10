854b7d7a2434ae90bc1cad51e6426611733654f9
crisjame09-stack <crisjame09@gmail.com>
2026-03-10 14:22:33 +0630

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,83 @@
+// Bubble Sort Algorithm in C++
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];   // Declare global array variable with size 20
+int n;         // Declare global variable n to store number of elements
+
+void input()
+{
+    // procedure for input
+    int i;
+
+    while (true)
+    {
+        cout << "Enter number of elements in the array: ";   // Output to screen
+        cin >> n;                                            // Input from user
+
+        if (n <= 20)                                         // If n is less than or equal to 20
+            break;                                           // Exit the loop
+        else
+            cout << "\nArray can have a maximum of 20 elements.\n"; // Output to screen
+    }
+
+    cout << endl;
+    cout << "Enter Array Elements" << endl;
+    cout << endl;
+
+    for (i = 0; i < n; i++)        // Loop from 0 to n-1
+    {
+        cout << "Data element-" << (i + 1) << " : ";  // Output to screen
+        cin >> arr[i];                                // Input from user
+    }
+}
+
+void bubbleSortArray()
+{
+    // procedure to sort array using bubble sort method
+    int pass = 1;
+    int j, k, temp;
+
+    do
+    {
+        for (j = 0; j <= n - 1 - pass; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+        cout << "\nPass " << pass << " : ";
+        for (k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+
+        pass++;
+
+    } while (pass <= n - 1);
+}
+
+void display()
+{
+    cout << "\n\nSorted array:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+
+    return 0;
+}
